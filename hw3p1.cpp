3911134a80275acaf5be8055943173e0a9e9f9fd
Kelsey Cole <krco253@cs.uky.edu>
2019-10-16 08:55:48 -0400

    okay

--- hw3p1.cpp
+++ hw3p1.cpp
@@ -17,7 +17,7 @@ void transpose(matrix &arr, matrix &result, int size){
 
 }
 //Forward substitution
-void forward_sub(matrix &A, vector<float> &b, vector<float> &y, int size){
+/*void forward_sub(matrix &A, vector<float> &b, vector<float> &y, int size){
 	for (int i = 0; i < size; i++){
 		float sum = 0;
 		for (int j = 0; j < i; j++){
@@ -25,9 +25,8 @@ void forward_sub(matrix &A, vector<float> &b, vector<float> &y, int size){
 		}
 		y[i] = (b[i] - sum)/ A[i][i];
 	}
-}
+}*/
 //--------------CHOLESKY's METHOD----------------
-//template<typename T, size_t size>
 void CholeskysMethod(matrix &A, int n, matrix &lower_matrix){
 
 	//--------------Initialize L 
@@ -37,7 +36,7 @@ void CholeskysMethod(matrix &A, int n, matrix &lower_matrix){
 		lower_matrix[i][j] = 0;
 		}
 	}
-	//-------------
+	//------------comupte diagonal
 	for (int k = 0; k < n; k++){
 		float sum = 0;
 		for (int s = 0; s < k; s++){
@@ -45,7 +44,7 @@ void CholeskysMethod(matrix &A, int n, matrix &lower_matrix){
 		}
 		lower_matrix[k][k] = sqrt(A[k][k] + sum);
 
-		//-----------
+		//-----------compute the rest
 		for (int i = k+1; i < n; i++){
 			float sum2 = 0;
 			for (int s = 0; s < k; s++){
@@ -83,19 +82,28 @@ for (int size = 20; size < 120; size+=20){
 	matrix lower_transpose (size, vector<float>(size, 0));
 	transpose(lower, lower_transpose, size);
 //forward substitution
-	vector<float> y (size, 0);
-	vector<float> b (size, 6);
-	b[0] = 5;
-	b[size - 1] = 5;
-	forward_sub(array, y, b, size);
+//	vector<float> y (size, 0);
+//	vector<float> b (size, 6);
+//	b[0] = 5;
+//	b[size - 1] = 5;
+//	forward_sub(array, y, b, size);
 	if (size == 20){	
-	//cout << "Size: " << size << endl << endl;
+	cout << "Size: n = " << size << endl << "L computed by Cholesky: " << endl;
 	for (int i = 0; i < size; i++){
-		//for (int j = 0; j < size; j++){
-			cout << y[i] << " ";
-		//}
+		for (int j = 0; j < size; j++){
+			cout << lower[i][j] << " ";
+		}
 		cout << endl;
 	}
+	cout << endl;
+	cout<< "L transpose: " << endl << endl;
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++){
+			cout << lower_transpose[i][j] << " ";
+		}
+		cout << endl;
+	}
+
 	}
 
 }
